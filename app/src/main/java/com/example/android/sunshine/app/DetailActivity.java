06f7178ec79cbf837c97e0db7633149b1c67b641
Kathryn Newbould <kathrynnewbould@yahoo.com>
2016-01-23 15:04:46 +0000

    Created Detail Activity

---
+++ app/src/main/java/com/example/android/sunshine/app/DetailActivity.java
@@ -0,0 +1,15 @@
+package com.example.android.sunshine.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Kathr on 23/01/2016.
+ */
+public class DetailActivity extends Activity{
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+
+}
